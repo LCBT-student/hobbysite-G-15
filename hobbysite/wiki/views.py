08ff2edda39d936184ee436ac14fceb4422e0756
LCBT <cedric.tan@student.ateneo.edu>
2024-03-20 19:39:53 +0800

    Created wiki views

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import ArticleCategory, Article
+
+
+class ArticleListView(ListView):
+    model = ArticleCategory
+
+    template_name = 'wiki/article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+    template_name = 'wiki/article_view.html'
