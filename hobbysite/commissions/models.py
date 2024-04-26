9b6cd622b6ee59caad27d17954c1ed6a5f079a27
Hans Zhuang <hans.zhuang@student.ateneo.edu>
2024-04-26 21:32:41 +0800

    renamed variables to be consistent

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -4,44 +4,44 @@ from django.urls import reverse
 # Create your models here.
 
 class Commission(models.Model):
-    Title = models.CharField(max_length=255)
-    Description = models.TextField()
-    People_Required = models.PositiveIntegerField()
-    Created_On = models.DateTimeField(auto_now_add=True)
-    Updated_On = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.Title
+        return self.title
     
     def get_absolute_url(self):
         return reverse('commissions:commission_detail', args=[self.id])
     
 
     class Meta:
-        ordering = ['Created_On']
+        ordering = ['created_on']
         verbose_name = 'commission'
         verbose_name_plural = 'commissions'
 
 
 class Comment(models.Model):
-    Commission = models.ForeignKey(
+    commission = models.ForeignKey(
         Commission,
         on_delete=models.CASCADE,
         related_name='comments'
     )
-    Entry = models.TextField()
-    Created_On = models.DateTimeField(auto_now_add=True)
-    Updated_On = models.DateTimeField(auto_now=True)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '{} {}'.format(Commission.Title, self.Entry)
+        return '{} {}'.format(Commission.title, self.entry)
 
     def get_absolute_url(self):
         return reverse('commission_detail', args=[self.id])
     
     
     class Meta:
-        ordering = ['-Created_On']
-        unique_together = ['Created_On', 'Entry']
+        ordering = ['-created_on']
+        unique_together = ['created_on', 'entry']
         verbose_name = 'comment'
         verbose_name_plural = 'comments'
