ca6665024f4190b6624b536c25b6112d8aa92b9d
LCBT <cedric.tan@student.ateneo.edu>
2024-05-05 16:40:37 +0800

    Updated project for media usage

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -125,10 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
+STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/homepage'
 LOGOUT_REDIRECT_URL = 'accounts/login'
-
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
