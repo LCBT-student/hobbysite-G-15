da67f2aafe6251666cd3f82da79ef1edcdf5f749
LCBT <cedric.tan@student.ateneo.edu>
2024-05-05 17:13:05 +0800

    Updated Article and added Comment model.

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+from user_management.models import Profile
+
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255, verbose_name='Category')
@@ -23,15 +25,23 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='article'
+    )
     category = models.ForeignKey(
         ArticleCategory,
         on_delete=models.SET_NULL,
         null=True,
-        related_name="category"
+        related_name='article',
+        limit_choices_to={'is_staff': True},
     )
     entry = models.TextField()
     created_on = models.DateTimeField(default=timezone.now)
-    updated_on = models.DateTimeField()
+    updated_on = models.DateTimeField(auto_now=True)
+    header_image = models.ImageField(upload_to='images/', null=True)
 
     def __str__(self):
         return self.title
@@ -39,9 +49,25 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse('wiki:article_detail_view', kwargs={'pk': self.pk})
 
-    def save(self, *args, **kwargs):
-        self.updated_on = timezone.now()
-        super().save(*args, **kwargs)
-
     class Meta:
         ordering = ['-created_on']
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='comment'
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='comment'
+    )
+    entry = models.TextField()
+    created_on = models.DateField(default=timezone.now)
+    updated_on = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
