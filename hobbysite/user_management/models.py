36f05b5cf5c200f04e55634c09ed68f37bc9997a
LCBT <cedric.tan@student.ateneo.edu>
2024-05-05 15:47:19 +0800

    Created Profile model

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=63)
+    email_address = models.EmailField()
